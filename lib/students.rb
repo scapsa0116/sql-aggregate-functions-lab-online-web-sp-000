974fbeb071a1c852560f692752cb6477c593e530
Stela Capsa <stela.capsa@gmail.com>
2020-02-17 14:37:48 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -5,17 +5,17 @@ def highest_student_gpa
 end
 
 def lowest_student_gpa
- puts  1.9
+ "SELECT MIN (gpa) FROM students;"
 end
 
 def average_student_gpa
- puts 2.95
+ "SELECT AVG (gpa) FROM students;"
 end
 
 def total_tardies_for_all_students
- puts 49
+ "SELECT SUM (tardies) FROM students;"
 end
 
 def average_gpa_for_9th_grade
-  puts 3.3
+"SELECT AVG (gpa) FROM students WHERE grade = 9;"
 end
