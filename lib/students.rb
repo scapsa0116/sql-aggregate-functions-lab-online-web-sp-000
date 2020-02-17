859275c175c23f31a79d3c644165820622936503
Stela Capsa <stela.capsa@gmail.com>
2020-02-17 14:33:22 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,7 +1,7 @@
 ## Code your solution below. Note that your SQL queries should be in quotation marks. 
 
 def highest_student_gpa
-puts 4.0
+SELECT MAX (gpa) FROM students;
 end
 
 def lowest_student_gpa
