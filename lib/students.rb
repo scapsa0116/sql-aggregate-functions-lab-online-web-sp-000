2e4571c91244fd306dbaa8d43df6f50b285dbf11
Stela Capsa <stela.capsa@gmail.com>
2020-02-17 14:30:35 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,21 +1,21 @@
 ## Code your solution below. Note that your SQL queries should be in quotation marks. 
 
 def highest_student_gpa
-puts "4.0"
+puts 4.0
 end
 
 def lowest_student_gpa
-  "1.9"
+  1.9
 end
 
 def average_student_gpa
-  "2.95"
+  2.95
 end
 
 def total_tardies_for_all_students
-  "49"
+  49
 end
 
 def average_gpa_for_9th_grade
-  "3.3"
+  3.3
 end
